71922a61312a5d344301b429fd5f02fc5a8f8aec
engnajemali-dot <eng.najem.ali@gmail.com>
2026-05-22 21:54:20 +0300

    add a new comment

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ print("Hello World")
 
 
 print("This is a test of the emergency broadcast system.")
+
+print ("This is only a test.")
+# This is a comment in Python. It is ignored by the interpreter.
